833db94a3f280c65afd7206801a869e2645d57e7
Mahdi <mahdimeyghani02@gmail.com>
2024-05-22 15:22:01 +0330

    Add URl endpoints for a particular year and all dates for a particular station

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 df_station = pd.read_csv("data/stations.txt", skiprows=17)
 
+
 @app.route("/")
 def home():
     return render_template("home.html",
@@ -30,6 +31,40 @@ def api(station, date):
                   "temperature": temperature}
 
     return dictionary
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    path = f"data/TG_STAID{str(station).zfill(6)}.txt"
+
+    df = pd.read_csv(path, skiprows=20)
+    df["   TG"] = df["   TG"] / 10
+
+    result = df.to_dict(orient="records")
+
+    for temperature in result:
+        if temperature["   TG"] == -999.9:
+            temperature["   TG"] = "LOST"
+
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def on_year(station, year):
+    path = f"data/TG_STAID{str(station).zfill(6)}.txt"
+
+    df = pd.read_csv(path, skiprows=20, parse_dates=["    DATE"])
+
+    df["   TG"] = df["   TG"] / 10
+
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(year)].to_dict(orient="records")
+
+    for temperature in result:
+        if temperature["   TG"] == -999.9:
+            temperature["   TG"] = "LOST"
+
+    return result
 
 
 if __name__ == "__main__":
