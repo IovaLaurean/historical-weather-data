5caf70f4b73e8b7dbb3ac7b2c8773bcdacb3efb5
LrN <iovalau112@yahoo.ro>
2025-05-25 16:08:21 +0300

    read files from bucket

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+import io
+
+import pandas as pd
 from flask import Flask
 from flask import render_template
-import pandas as pd
+from google.cloud import storage
 
 app = Flask(__name__)
 
-df_station = pd.read_csv("data/stations.txt", skiprows=17)
+
+def read_text_file_from_gcs(file_name):
+    # Initialize the client
+    client = storage.Client()
+
+    # Get the bucket
+    bucket = client.bucket('historical-weather-data')
+
+    # Get the blob (file)
+    blob = bucket.blob(file_name)
+
+    # Download the contents as a string
+    file_contents = blob.download_as_text()
+
+    return file_contents
+
+
+df_station = pd.read_csv(io.StringIO(read_text_file_from_gcs("stations.txt")), skiprows=17)
 
 
 @app.route("/")
@@ -15,9 +35,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
-    path = f"data/TG_STAID{station.zfill(6)}.txt"
+    path = f"TG_STAID{station.zfill(6)}.txt"
 
-    df = pd.read_csv(path, skiprows=20, parse_dates=["    DATE"])
+    df = pd.read_csv(io.StringIO(read_text_file_from_gcs(path)), skiprows=20, parse_dates=["    DATE"])
 
     date_df = df.loc[df["    DATE"] == date]
     raw_temperature = date_df["   TG"]
@@ -35,9 +55,9 @@ def api(station, date):
 
 @app.route("/api/v1/<station>")
 def all_data(station):
-    path = f"data/TG_STAID{str(station).zfill(6)}.txt"
+    path = f"TG_STAID{str(station).zfill(6)}.txt"
 
-    df = pd.read_csv(path, skiprows=20)
+    df = pd.read_csv(io.StringIO(read_text_file_from_gcs(path)), skiprows=20)
     df["   TG"] = df["   TG"] / 10
 
     result = df.to_dict(orient="records")
@@ -51,9 +71,9 @@ def all_data(station):
 
 @app.route("/api/v1/yearly/<station>/<year>")
 def on_year(station, year):
-    path = f"data/TG_STAID{str(station).zfill(6)}.txt"
+    path = f"TG_STAID{str(station).zfill(6)}.txt"
 
-    df = pd.read_csv(path, skiprows=20, parse_dates=["    DATE"])
+    df = pd.read_csv(io.StringIO(read_text_file_from_gcs(path)), skiprows=20, parse_dates=["    DATE"])
 
     df["   TG"] = df["   TG"] / 10
 
